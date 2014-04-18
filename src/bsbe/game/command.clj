ea721d8149e68e935ba839d2228e304415a9f289
xensky <xensky@gmail.com>
2014-04-17 22:28:34 -0400

    add support for command parsing using pushdown-automaton-esque process, and fn to extract dictionary from commands

--- src/bsbe/game/command.clj
+++ src/bsbe/game/command.clj
@@ -35,13 +35,18 @@
   (let [rules (parse-rules rules)
         actions (map convert-action actions)]
     ; make a new id for the command, and return the rules and actions
-    `{:id (gensym) :rules ~(vec rules) :actions ~(vec actions)}))
+    `{:id (keyword (gensym)) :rules ~(vec rules) :actions ~(vec actions)}))
 
 ; (make-commands [["this" | :that] "actions" (to take)]
 (defmacro make-commands
   "defines commands - rules for matching, and actions to take"
   [& commands]
   (vec (map #(apply parse-command %) commands)))
+
+(defn commands->dictionary
+  "extracts dictionary entries from commands"
+  [commands]
+  (into {} (map #(hash-map (:id %) (:rules %)) commands)))
 
 (defn parse-dictionary-entry
   "parses a dictionary entry [name [rules]]"
@@ -72,3 +77,61 @@
      (let [entities (map eval entities)]
        `(defn ~name []
           {:id (keyword '~name) :description ~description :entities ~entities :commands ~commands :state ~state}))))
+
+(defn non-terminal?
+  "determines if a symbol is non-terminal"
+  [symbol]
+  (keyword? symbol))
+
+(defn expand
+  "expands a non-terminal symbol's rules"
+  [expand-symbol commands inputs symbols]
+  (let [expansions (get commands expand-symbol) ; get the symbol expansions
+        ; add the remaining symbols to each of the new expansions
+        symbol-lists (map #(concat % symbols) expansions)
+        ; group the inputs with the symbol lists
+        new-parses (map #(list inputs %) symbol-lists)]
+    new-parses))
+
+(defn parse-command-grammar
+  "parses an input against a specific rule"
+  [inputs symbols commands]
+  (let [top-input (first inputs)
+        top-symbol (first symbols)
+        no-inputs (empty? inputs)
+        no-symbols (empty? symbols)]
+    (cond
+     ; success - both inputs and symbols empty
+     (and no-inputs no-symbols) true
+     ; nil/empty - just drop the nil symbol and continue
+     (= top-symbol nil) [[inputs (rest symbols)]]
+     ; match - input and symbol are equal
+     (= top-input top-symbol) [[(rest inputs) (rest symbols)]]
+     ; expand - non-terminal symbol, get its expansions
+     (non-terminal? top-symbol) (expand top-symbol commands inputs (rest symbols))
+     :else false)))
+
+(defn match-command
+  "determines if an input matches a command's rules"
+  [inputs rule commands]
+  (let [symbols [rule]] ; start symbol stack with the selected rule
+    (loop [inputs-symbols [[inputs symbols]]] ; list of input-symbol pairs
+      (if (empty? inputs-symbols) ; if no more pairs, didn't match
+        false
+        (let [[inputs symbols] (first inputs-symbols) ; get first pair
+              parsed (parse-command-grammar inputs symbols commands)] ; parse it
+          (cond
+           ; if seq, still more pairs to parse
+           (seq? parsed) (recur (concat (rest inputs-symbols) parsed))
+           ; if parsed, found match
+           parsed true
+           ; else no match this branch, continue checking other pairs
+           :else (recur (rest inputs-symbols))))))))
+
+(defn find-best-match
+  "finds the best matching command for an input"
+  [inputs commands]
+  (let [symbols (keys commands) ; check each symbol in commands
+        ; find the first that matches the input
+        match (first (filter #(match-command inputs % commands) symbols))]
+    match))
