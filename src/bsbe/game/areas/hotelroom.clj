b030d0891f7b1bb96b3ab2f8a5439c2926ed2f35
xensky <xensky@gmail.com>
2014-04-19 17:34:18 -0400

    separate defarea/entity into util, add some other utility fns

--- src/bsbe/game/areas/hotelroom.clj
+++ src/bsbe/game/areas/hotelroom.clj
@@ -1,6 +1,7 @@
 (ns bsbe.game.areas.hotelroom
   (:use [bsbe.game.command]
-        [bsbe.game.actions]))
+        [bsbe.game.actions]
+        [bsbe.game.util]))
 
 (defentity hotelbed
   (make-commands [[:look "bed"] "It's a well aged hotel bed. The blanket has an atrocious mixture of brown and green pattern. The springs squeek with the sigh of a decade's use."]))
--- src/bsbe/game/command.clj
+++ src/bsbe/game/command.clj
@@ -59,26 +59,6 @@
   [& dictionary-entries]
   (into {} (map parse-dictionary-entry dictionary-entries)))
 
-; (defentity thing (make-commands) state)
-(defmacro defentity
-  "defines an entity"
-  ([name commands]
-     `(defentity ~name ~commands {}))
-  ([name commands state]
-     `(defn ~name []
-        {:id (keyword '~name) :commands ~commands :state ~state})))
-
-; (defarea place "description" [entities] (make-commands) state)
-(defmacro defarea
-  "defines an area"
-  ([name description entities commands]
-     `(defarea ~name ~description ~entities ~commands {}))
-  ([name description entities commands state]
-     ; evaluate the entity fns to create the entity list
-     (let [entities (vec (map list entities))]
-       `(defn ~name []
-          {:id (keyword '~name) :description ~description :entities ~entities :commands ~commands :state ~state}))))
-
 (defn non-terminal?
   "determines if a symbol is non-terminal"
   [symbol]
