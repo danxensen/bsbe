ec21b7739be1db11ff8ddec429545fdeaab7f183
xensky <xensky@gmail.com>
2014-04-25 22:16:25 -0400

    display current location description on screen

--- src/bsbe/gui/uis/game.clj
+++ src/bsbe/gui/uis/game.clj
@@ -6,6 +6,9 @@
   "renders the game state"
   [stage state]
   (let [partial-input (Label. (apply str ">" (:partial-input state)) @style)
+        location (Label. (str "@ " (get-in state [:areas (:location state) :description])) @style)
         message (:animated-message state)]
     (.addActor @stage partial-input)
+    (.setY location first-line-y)
+    (.addActor @stage location)
     (format-message message stage)))
